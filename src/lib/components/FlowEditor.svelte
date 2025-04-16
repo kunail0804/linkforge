a738a5ba69f07d21cac114345e3bc6a4eec45a43
Crespin Alexandre <kunail0804@gmail.com>
2025-04-16 20:59:49 +0200

    Initial commit

---
+++ src/lib/components/FlowEditor.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { writable } from 'svelte/store';
+    import { SvelteFlow, Background, Controls } from '@xyflow/svelte';
+  
+    const nodes = writable([
+      { id: '1', position: { x: 100, y: 100 }, data: { label: 'Start' }, type: 'default' }
+    ]);
+  
+    const edges = writable([]);
+  </script>
+  
+  <SvelteFlow {nodes} {edges} style="height: 100%; width: 100%;">
+    <Background />
+    <Controls />
+  </SvelteFlow>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<!-- src/routes/+page.svelte -->
+<script>
+    import FlowEditor from '$lib/components/FlowEditor.svelte';
+  </script>
+  
+  <main style="height: 100vh">
+    <FlowEditor />
+  </main>
+  
